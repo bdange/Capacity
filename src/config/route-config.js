56d1b8ac48e009dc979c5faa22cc420eea303a12
bdange <benjamin.dange@gmail.com>
2019-10-15 22:48:14 +0200

    commented out all routes except static for testing purpose

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -1,15 +1,15 @@
 module.exports = {
   init(app) {
     const staticRoutes = require("../routes/static");
-    const prosRoutes = require("../routes/pros");
-    const clientRoutes = require("../routes/client");
-    const userRoutes = require("../routes/users");
-    const listingRoutes = require("../routes/listings");
+    // const prosRoutes = require("../routes/pros");
+    // const clientRoutes = require("../routes/client");
+    // const userRoutes = require("../routes/users");
+    // const listingRoutes = require("../routes/listings");
 
     app.use(staticRoutes);
-    app.use(prosRoutes);
-    app.use(clientRoutes);
-    app.use(userRoutes);
-    app.use(listingRoutes);
+    // app.use(prosRoutes);
+    // app.use(clientRoutes);
+    // app.use(userRoutes);
+    // app.use(listingRoutes);
   }
 };
